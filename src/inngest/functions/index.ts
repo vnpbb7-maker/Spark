fbf732f8bd16eff8c9454a959f30e83ccfe47afc
vnpbb7-maker <vnpbb7-maker@users.noreply.github.com>
2026-04-30 15:43:50 +0900

    fix: inngest v4 correct triggers syntax

--- src/inngest/functions/index.ts
+++ src/inngest/functions/index.ts
@@ -1,37 +1,28 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { inngest } from "../client";
 
-// @ts-ignore
 export const discoverTargets = inngest.createFunction(
-  { id: "discover-targets" },
-  { event: "campaign/discover" },
+  { id: "discover-targets", triggers: [{ event: "campaign/discover" }] },
   async ({ event }: any) => {
     return { success: true };
   }
 );
 
-// @ts-ignore
 export const generateComments = inngest.createFunction(
-  { id: "generate-comments" },
-  { event: "campaign/generate" },
+  { id: "generate-comments", triggers: [{ event: "campaign/generate" }] },
   async ({ event }: any) => {
     return { success: true };
   }
 );
 
-// @ts-ignore
 export const postComments = inngest.createFunction(
-  { id: "post-comments" },
-  { event: "campaign/post" },
+  { id: "post-comments", triggers: [{ event: "campaign/post" }] },
   async ({ event }: any) => {
     return { success: true };
   }
 );
 
-// @ts-ignore
 export const monitorReplies = inngest.createFunction(
-  { id: "monitor-replies" },
-  { event: "campaign/monitor" },
+  { id: "monitor-replies", triggers: [{ event: "campaign/monitor" }] },
   async ({ event }: any) => {
     return { success: true };
   }
